ab66287b56ea9fb2a04759159850c0c4e406d3fb
tiabeanie <sydney.jane.hunter@gmail.com>
2021-05-26 18:52:13 -0600

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ document.getElementById('main').remove()
 
 const newHeader = document.createElement('h1')
 newHeader.id = "victory"
-newHeader.innerHTML = "YOUR-NAME is the champion"
+newHeader.innerHTML = "Syd is the champion"
